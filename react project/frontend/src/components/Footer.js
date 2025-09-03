447ee6b9d591d608c4f9b50fa85cff4269e59805
unknown <sagabalasaicharan@gmail.com>
2025-09-03 16:38:17 +0530

    Initial commit: Added SpringBoot Project, project notes, and react project directories

---
+++ react project/frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+import '../App.css';
+function Footer()
+{
+    return (
+        <div className="footer">
+               <small> All Right Reserved &copy;2025  </small> 
+        </div>
+    )
+}
+export default Footer;
